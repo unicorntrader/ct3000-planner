b95c48c7152d881dd4891859f5d0550532d6a467
unicorntrader <antonis@protopapas.net>
2025-08-02 16:38:20 +0300

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,4 @@
-import { addMessage } from "../../../lib/webhookStore";
+import { addMessage } from "../../lib/webhookStore";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
